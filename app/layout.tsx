5ec341ebdb781c66503dc9e79f52b8a8fc9c32ba
joelriveradev <joelriveradev@gmail.com>
2024-05-28 13:49:18 -0500

    feat: integrate clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+import Link from 'next/link'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,7 +28,24 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <header className='w-full text-lg px-8 py-5 border-b border-b-neutral-200'>
+            <nav className='w-full flex items-center justify-between max-w-7xl mx-auto'>
+              <Link href='/' className='font-bold'>
+                arise
+              </Link>
+
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </nav>
+          </header>
+          <div className='max-w-7xl mx-auto>'>{children}</div>
+        </body>
       </html>
     </ClerkProvider>
   )
