c9851e66a3c345a307d13a99f8db9ef996d2956c
joelriveradev <joelriveradev@gmail.com>
2024-06-05 21:25:01 -0500

    chore: add border style

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function HomePage() {
   const { quarterlies } = await getQuarterlies()
 
   return (
-    <main className='w-full h-dvh grid grid-cols-1 justify-items-center md:grid-cols-2 md:justify-items-start xl:grid-cols-3 2xl:grid-cols-4 px-8 py-12 xl:px-0'>
+    <main className='w-full h-dvh grid grid-cols-1 justify-items-center md:grid-cols-2 md:justify-items-start xl:grid-cols-3 2xl:grid-cols-4 px-8 py-12 lg:px-10 lg:border-x'>
       {quarterlies.map(({ id, title, quarter, cover, year }) => {
         return (
           <Link
