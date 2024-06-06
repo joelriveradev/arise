3162ed422d6f27566e596923349263d20700b4d4
joelriveradev <joelriveradev@gmail.com>
2024-06-05 21:24:40 -0500

    feat: integrate accordion in weeks, h1 style tweaks

--- app/quarterly/[year]/[quarter]/page.tsx
+++ app/quarterly/[year]/[quarter]/page.tsx
@@ -1,4 +1,14 @@
 import { getQuarterlyByYear } from '@/actions/quarterlies'
+import { Libre_Caslon_Text } from 'next/font/google'
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+
+import { cn } from '@/lib/utils'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -10,6 +20,11 @@ interface Props {
   }
 }
 
+const CaslonText = Libre_Caslon_Text({
+  subsets: ['latin'],
+  weight: ['400'],
+})
+
 export default async function QuarterlyOverviewPage({ params }: Props) {
   const { year, quarter } = params
 
@@ -20,8 +35,8 @@ export default async function QuarterlyOverviewPage({ params }: Props) {
   const { title, cover, introduction, weeks } = quarterlies[0]
 
   return (
-    <main className='w-full flex items-start pt-0 md:pt-12 md:pr-8 xl:pr-0'>
-      <aside className='hidden md:flex lg:w-1/6 md:ml-8 xl:m-0'>
+    <main className='w-full h-[calc(100dvh-70px)] flex items-start justify-center pt-0 md:pt-12 md:pr-8 xl:pr-0 border-x'>
+      <aside className='hidden h-full md:flex md:flex-col md:items-center lg:w-1/6 md:ml-8 xl:m-0'>
         <Image
           className='max-w-[180px] rounded-2xl'
           src={cover.url}
@@ -32,31 +47,65 @@ export default async function QuarterlyOverviewPage({ params }: Props) {
           placeholder='blur'
           priority
         />
+
+        <p className='mt-3'>
+          Q{quarter} {year}
+        </p>
       </aside>
 
-      <div className='w-full md:w-3/4 p-8 md:px-0 md:pl-8 md:py-0'>
-        <h1 className='font-semibold text-2xl md:text-3xl'>{title}</h1>
+      <div className='w-full md:w-3/4 p-8 md:px-0 md:pl-10 lg:pl-12 md:py-0'>
+        <h1
+          className={cn(
+            'font-semibold text-2xl md:text-3xl lg:text-4xl',
+            CaslonText.className
+          )}
+        >
+          {title}
+        </h1>
 
         <h2 className='mt-6 mb-2'>Introduction</h2>
 
-        <p className='antialised text-neutral-500 max-w-3xl'>
+        <p className='antialised text-neutral-600 max-w-3xl'>
           {introduction?.text}
         </p>
 
-        <div className='mt-8'>
-          <ol>
-            {weeks.map(({ id, title, number }, i) => {
+        <div className='mt-8 border-t'>
+          <Accordion type='single' collapsible>
+            {weeks.map(({ id, title, number, lessons }, i) => {
               return (
-                <li key={id} className='border-t border-t-neutral-200 py-5'>
-                  <Link href={`${quarter}/${number}/1`} prefetch>
-                    <span className='text-lg antialiased'>
-                      Week {i + 1}: <strong>{title}</strong>
-                    </span>
-                  </Link>
-                </li>
+                <AccordionItem value={id} key={id} className=''>
+                  <AccordionTrigger className='hover:bg-neutral-50 px-5'>
+                    <Link href={`${quarter}/${number}/1`} prefetch>
+                      <span className='text-sm lg:text-lg antialiased text-left font-medium'>
+                        Week {i + 1}: {title}
+                      </span>
+                    </Link>
+                  </AccordionTrigger>
+
+                  <AccordionContent>
+                    <ul>
+                      {lessons.map((lesson, i) => {
+                        return (
+                          <li
+                            key={lesson.id}
+                            className='mb-4 pl-8 lg:pl-12 text-neutral-600 last:mb-1.5'
+                          >
+                            <Link
+                              href={`${quarter}/${number}/${lesson.number}`}
+                              className='hover:underline hover:underline-offset-4'
+                              prefetch
+                            >
+                              Day {i + 1}: {lesson.title}
+                            </Link>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
               )
             })}
-          </ol>
+          </Accordion>
         </div>
       </div>
     </main>
